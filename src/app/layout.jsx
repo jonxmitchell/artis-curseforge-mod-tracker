6d23c51a7cc0fa765b7c6f758d8567c5e3d9e686
Jon Mitchell <jonxmitchell@outlook.com>
2024-11-01 11:45:59 +0000

    Removed themeprovider

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,16 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body className="min-h-screen bg-background text-foreground">
-        <NextUIProvider>
-          <NextThemesProvider attribute="class" defaultTheme="dark">
-            {children}
-          </NextThemesProvider>
-        </NextUIProvider>
+      <body className={`${inter.className} min-h-screen bg-background text-foreground`}>
+        <NextUIProvider>{children}</NextUIProvider>
       </body>
     </html>
   );
